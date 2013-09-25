34643ee464dc067c304bbf7a8f1fa96a208cb9b6
Nikolina Lauc <ninalauc@gmail.com>
2013-09-25 11:26:02 +0100

    Adding missing files

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require 'rubygems'
 require 'sinatra'
+require 'data_mapper'
+
+ # If you want the logs displayed you have to do this before the call to setup
+  DataMapper::Logger.new($stdout, :debug)
+
+  # A MySQL connection:
+  DataMapper.setup(:default, 'mysql://localhost/test')
+
 
 
 get '/hello' do
---
+++ comment.rb
@@ -0,0 +1,11 @@
+class Comment
+  include DataMapper::Resource
+
+  property :id,         Serial
+  property :posted_by,  String
+  property :email,      String
+  property :url,        String
+  property :body,       Text
+
+  belongs_to :post
+end
